31f5d4699780f19ceda5c12184920c32161d8a82
crakcode <59104583+crakcode@users.noreply.github.com>
2021-02-02 19:58:23 +0900

    first commit

---
+++ Industry_backend/src/main/java/com/Industry/IndustryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Industry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndustryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndustryBackendApplication.class, args);
+	}
+
+}
