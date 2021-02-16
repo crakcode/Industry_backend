ab4765a29020b37814180ceaf6f98902a6eab51f
신현중 <estein92@forcs.com>
2021-02-16 13:10:05 +0900

    News Controller Merge

--- Industry_backend/src/main/java/com/industry/rest/news/NewsCtrl.java
+++ Industry_backend/src/main/java/com/industry/rest/news/NewsCtrl.java
@@ -1,5 +1,5 @@
 package com.industry.rest.news;
-<<<<<<< HEAD
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,7 +32,6 @@ public class NewsCtrl {
 	
 	//HashMap<Integer,String> map = new HashMap<>();//new에서 타입 파라미터 생략가능
 	//map.put(1,"사과");
-	
 	
     @GetMapping("")
     public List<String> getNews() {
@@ -96,46 +95,6 @@ public class NewsCtrl {
 //			return hash;
 //			}
 //		return hash;
-
-
-    
-    @GetMapping("/companys")
-    public List<String> ComapyReturnName() {
-    	List<String> map=new ArrayList();
-    	for(int i=0;i<companyResp.findAll().size();i++) {
-            String company=companyResp.findAll().get(i).getCompanyName();
-            map.add(company);
-    	}
-        return map;
-    }
-=======
-
-import lombok.RequiredArgsConstructor;
-
-import java.io.IOException;
-import java.util.List;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import com.industry.dao.community.CommunityService;
-import com.industry.dao.post.PostService;
-import com.industry.dto.community.CommunityTO;
-import com.industry.entity.Community;
-import com.industry.entity.Post;
-
-@CrossOrigin(origins = "http://localhost:3000")
-@RestController
-@RequestMapping("/api/v1/news")
-public class NewsCtrl {
-	
-	  @Autowired
-	  private PostService postServ;
-
 	  @GetMapping("")
 	  public void getAllPost(){
 			String url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=산업기능요원";
@@ -148,31 +107,17 @@ public class NewsCtrl {
 				e.printStackTrace();
 			}
 	  }
-//
-	  @GetMapping("/writer/{ucode}")
-	  public List<Post> getPostByWriter(@PathVariable Long ucode){
-		  	return postServ.getPostByWriter(ucode);
-	  }
-	  
-	  @PostMapping("/{ucode}")
-	  public void createPost(@RequestBody Post post,@PathVariable Long ucode) {
-	  	postServ.createPost(post,ucode);
-	  }
 
-	  @GetMapping("/{id}")
-	  public ResponseEntity<Post> getPostById(@PathVariable Long id){
-		  return postServ.getPostById(id);
-	  }
-//    
-//    
-	  @PutMapping("/{id}")
-	  public ResponseEntity<Post> updatePost(@PathVariable Long id,@RequestBody Post post){
-		  return postServ.updatePost(id,post);
-	  }
 
-	  @DeleteMapping("/{id}")
-	  public void deletePost(@PathVariable Long id) {
-		  postServ.deletePost(id);
-	  }
->>>>>>> branch 'master' of https://github.com/crakcode/Industry_backend.git
+    
+    @GetMapping("/companys")
+    public List<String> ComapyReturnName() {
+    	List<String> map=new ArrayList();
+    	for(int i=0;i<companyResp.findAll().size();i++) {
+            String company=companyResp.findAll().get(i).getCompanyName();
+            map.add(company);
+    	}
+        return map;
+    }
+
 }
